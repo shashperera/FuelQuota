ab5800b9e010f5eb98ae54389a93f5e79472cc71
shashperera <sanduni.p@sliit.lk>
2022-10-07 10:56:28 +0530

    Update Quota

---
+++ Vehicle/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    aws_table_name: 'vehicleR_registration',
+    aws_local_config: {
+        //Provide details for local configuration
+    },
+    aws_remote_config: {
+        accessKeyId: 'AKIAX3W627NSCYQ4ZWUC',
+        secretAccessKey: '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',
+        region: 'us-east-1',
+    }
+};
